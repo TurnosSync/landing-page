2b0f216917fbfa855cdbef87b2882f2ed00a4b70
Juan David Cutiva <69434726+Juan-Cutiva@users.noreply.github.com>
2024-11-12 18:17:19 -0500

    [WORK] HeaderMobile

--- src/scripts/HeaderShow.ts
+++ src/scripts/HeaderShow.ts
@@ -1,37 +1,39 @@
-const button = document.getElementById("burger") as HTMLInputElement;
-const header = document.getElementById("headerMobile") as HTMLElement;
-const navItems = document.getElementById("navMobile") as HTMLElement;
+document.addEventListener("astro:page-load", () => {
+  const button = document.getElementById("burger") as HTMLInputElement;
+  const header = document.getElementById("headerMobile") as HTMLElement;
+  // const navItems = document.getElementById("navMobile") as HTMLElement;
 
-button.addEventListener("click", activate);
+  button.addEventListener("click", activate);
 
-function activate(): void {
-  if (button.checked === true) {
-    header.style.display = "flex";
-    header.style.justifyContent = "flex-end";
-    navItems.style.display = "flex";
-    header.style.animation = "fade-in .5s forwards";
-  } else {
-    navItems.style.display = "none";
-    header.style.animation = "fade-out .5s forwards";
+  function activate(): void {
+    if (button.checked === true) {
+      header.style.display = "flex";
+      header.style.justifyContent = "flex-end";
+      //navItems.style.display = "flex";
+      header.style.animation = "fade-in .3s forwards";
+    } else {
+      //navItems.style.display = "none";
+      header.style.animation = "fade-out .3s forwards";
+      setTimeout(() => {
+        header.style.display = "none";
+      }, 300);
+    }
+  }
+
+  document.querySelector("main")?.addEventListener("click", () => {
+    //navItems.style.display = "none";
+    header.style.animation = "fade-out .3s forwards";
     setTimeout(() => {
       header.style.display = "none";
     }, 300);
-  }
-}
-
-document.querySelector("main")?.addEventListener("click", () => {
-  navItems.style.display = "none";
-  header.style.animation = "fade-out .5s forwards";
-  setTimeout(() => {
-    header.style.display = "none";
-  }, 300);
-  button.checked = false;
-});
-
-window.addEventListener("resize", () => {
-  if (window.innerWidth >= 764) {
-    navItems.style.display = "none";
-    header.style.display = "none";
     button.checked = false;
-  }
+  });
+
+  window.addEventListener("resize", () => {
+    if (window.innerWidth >= 764) {
+      //navItems.style.display = "none";
+      header.style.display = "none";
+      button.checked = false;
+    }
+  });
 });
